452309b6faaef67e6971bdff4ee3d9713b8a9ec5
Neel Patel <neel199907@gmail.com>
2022-06-05 22:35:12 +0530

    build the UI components

---
+++ scripts/issue-token.js
@@ -0,0 +1,9 @@
+const TokenFarm = artifacts.require("TokenFarm");
+
+module.exports = async function(callback) {
+  let tokenFarm = await TokenFarm.deployed();
+  await tokenFarm.issueToken();
+
+  console.log("Tokens issued!!");
+  callback();
+};
