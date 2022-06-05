90343999b06e575d272a739d085fc3e94d0ee991
Neel Patel <neel199907@gmail.com>
2022-06-05 17:49:22 +0530

    addeed all smart contract functionality

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -7,9 +7,49 @@ contract TokenFarm {
     string public name = "Token Farm Contract";
     DappToken public dappToken;
     DaiToken public daiToken;
+    address public owner;
+
+    address[] stakers;
+    mapping(address => uint256) stakingBalance;
+    mapping(address => bool) hasStaked;
+    mapping(address => bool) isStaking;
 
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
+    }
+
+    function stakeTokens(uint256 _amount) public {
+        require(_amount > 0, "amount cannot be 0");
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    function issueToken() public {
+        require(msg.sender == owner, "caller must be owner");
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            uint256 balance = stakingBalance[recipient];
+
+            if (balance > 0) {
+                dappToken.transfer(recipient, balance);
+            }
+        }
+    }
+
+    function unstakeTokens() public {
+        uint256 balance = stakingBalance[msg.sender];
+        require(balance > 0, "staking balance cannot be less than 0");
+        daiToken.transfer(msg.sender, balance);
+        stakingBalance[msg.sender] = 0;
+        isStaking[msg.sender] = false;
     }
 }
